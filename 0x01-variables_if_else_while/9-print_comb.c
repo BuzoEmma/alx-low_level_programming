9fdce0ba210d526070be01d5b40bec41c6784f38
Buzo <chibuzoemmanuel55@gmail.com>
2023-09-10 13:18:54 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is main function that print comb
+ * Return: always return 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
