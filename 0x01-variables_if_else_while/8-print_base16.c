04cc930bf13fac7803fdd3a0b611faf2aa77293c
Buzo <chibuzoemmanuel55@gmail.com>
2023-09-10 12:30:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is main function to print base16
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
