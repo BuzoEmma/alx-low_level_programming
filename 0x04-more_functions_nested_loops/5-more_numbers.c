a2cef0db6d5c0002f4e32accdcc64a82c1c5d7cd
Buzo <chibuzoemmanuel55@gmail.com>
2023-09-17 10:24:03 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to print 10X of 1 - 14
+ * Return: always on 0 (sucess)
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
