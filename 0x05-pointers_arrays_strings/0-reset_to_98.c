31079cb34afdbeb96ad91538c41cead473974fdf
Buzo <chibuzoemmanuel55@gmail.com>
2023-09-19 09:53:41 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this is a function
+ * @n:the parameter
+ * Return: Always 0.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Working on my 0x05-pointers_arrays_string lesson
