42f72576687384d829b9d9f5aa3c23f314e218c4
Buzo <chibuzoemmanuel55@gmail.com>
2023-09-22 06:11:00 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - function that changes all lowercase
  * @n: function parameter
- * Return: n
+ * Return: dest
 */
 
 char *string_toupper(char *n)
@@ -13,9 +13,10 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'Z')
-			n[i] = n[i] - 32;
-		i++;
+	if (n[i] >= 'a' && n[i] <= 'z')
+	n[i] = n[i] - 32;
+	i++;
 	}
 	return (n);
 }
+
