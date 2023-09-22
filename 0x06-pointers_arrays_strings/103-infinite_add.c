d3316b7006dfa52ad3c6bceda7ee0a825c472a96
Buzo <chibuzoemmanuel55@gmail.com>
2023-09-22 08:39:10 -0700

    103-add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse array
+ * @n: integer parameter
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * infinite_add - this function adds 2 int as strings
  * @n1: first value number as a string
  * @n2: second value number as a string
@@ -10,44 +36,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, carry = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	i = j = k = 0;
-
-	while (n1[i] || n2[j])
-	{
-		int digit1 = (n1[i]) ? (n1[i] - '0') : 0;
-		int digit2 = (n2[j]) ? (n2[j] - '0') : 0;
-
-		int sum = digit1 + digit2 + carry;
-
-		if (k >= size_r - 1)
-			return (0);
-
-		r[k] = (sum % 10) + '0';
-		carry = sum / 10;
-
+	while (*(n1 + i) != '\0')
 		i++;
+	while (*(n2 + j) != '\0')
 		j++;
-		k++;
-	}
-
-	if (carry && k < size_r - 1)
-
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		r[k] = carry + '0';
-		k++;
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	r[k] = '\0';
-	if (k >= size_r)
-	return (0);
-
-	for (i = 0, j = k - 1; i < j; i++, j--)
-	{
-		char temp = r[i];
-
-		r[i] = r[j];
-		r[j] = temp;
-	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
+
