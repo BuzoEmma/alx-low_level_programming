64e534c65351c9f7d2d1e1d807261404451c40ca
Buzo <chibuzoemmanuel55@gmail.com>
2023-10-02 15:27:51 -0700

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * main - this function prints it self
+ * main - this main function prints it name
  * @argc: input value
- * @argv: inout value
- * Return: always 0
+ * @argv: input value
+ * Return: always 0 (success)
 */
 
 int main(int argc __attribute__((unused)), char *argv[])
